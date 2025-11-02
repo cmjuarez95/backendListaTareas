df133b8f3ced6140f8ab5c4ee4319f76f96acdb6
Mariano Juarez <cm.juarez.95@gmail.com>
2025-11-02 19:32:15 -0300

    cambie un detalle en config.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 import router from "./src/routes/index.routes.js";
 import Server from "./src/server/config.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
+
 
 const server = new Server();
 
 //agregar las rutas
 server.app.use('/api', router)
 
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-server.app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
-  });
 server.listen()
